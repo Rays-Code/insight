92c1f87019e88807f0f48a418447fe300dae2d9b
Dipraj Ray <raydipraj1234@gmail.com>
2025-05-03 21:14:48 +0530

    revamped UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Signin from "./pages/Signin"
 import Blog from "./pages/Blog"
 import Blogs from "./pages/Blogs"
 import Publish from "./pages/Publish"
+import HomePage from "./pages/HomePage"
 
 
 const App = () => {
@@ -11,7 +12,7 @@ const App = () => {
     <div>
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Blogs />}></Route>
+      <Route path="/" element={<HomePage />}></Route>
       <Route path="/signup" element={<Signup />}></Route>
       <Route path="/signin" element={<Signin />}></Route>
       <Route path="/blog/:id" element={<Blog />}></Route>
--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -11,7 +11,7 @@ const Appbar = ({name}: {name: string}) => {
   return (
     <div className='flex justify-between border-b px-10 py-4'>
 
-      <div className=' text-lg font-semibold flex flex-col justify-center'><Link to={"/blogs"}>Medium Blog</Link></div>
+      <div className=' text-2xl font-semibold flex flex-col justify-center'><Link to={"/"}>Insight Blog</Link></div>
       
       <div>
       <Link to={"/publish"}><button type="button" className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">New</button></Link>
